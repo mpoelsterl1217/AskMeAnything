de8521b3b393d1933374c1f668d9b72d0b8347df
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-07-19 17:18:46 -0500

    Finished MVP
    Image switches on button press

--- AskMeAnything/ViewController.swift
+++ AskMeAnything/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Globals
+    var ballNameArray = ["Certain", "ConcentrateAskAgain", "OutlookNotGood", "SignsYes", "SourcesNo"]
+    var randomNum: Int = 0
+    func newBallImage() {
+        randomNum = Int.random(in: 0...4)
+        ballImageView.image = UIImage(named: ballNameArray[randomNum])
+    }
+    
+    //Outlets
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //Actions
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
 }
 
